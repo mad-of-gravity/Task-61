4513988b3a919b72c506e3566471ef8487207bbe
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-15 16:32:40 +0200

    fix bugs

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const hotCollection = document.querySelectorAll("p.hot");
+  const hotCollection = document.querySelectorAll("p .price .hot");
 
   hotCollection.array.forEach(p => {
     p.textContent += "🔥";
