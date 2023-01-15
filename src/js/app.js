ac7756c84aa10b3bd1677c0d35475b8e15f882b8
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-15 16:15:35 +0200

    use querySelector

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const hotCollection = document.getElementsByClassName("hot");
+  const hotCollection = document.querySelectorAll("p.hot");
 
   hotCollection.array.forEach(p => {
     p.textContent += "🔥";
